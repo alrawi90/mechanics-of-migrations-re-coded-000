278f99ed068e9ba7901a619ab29881eec9810c72
ALI ALRAWI <ali.developer90@gmail.com>
2016-11-14 06:51:46 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ require 'active_record'
 require 'yaml/store'
 require 'ostruct'
 require 'date'
+require_relative "../artist.rb"
 
 require 'bundler/setup'
 Bundler.require
@@ -16,5 +17,3 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists-#{ENV['SINATRA_ENV']}.sqlite"
 )
-
-require_relative "../artist.rb"
